53fa674d8b95638bb8b40793ac72ea2633016319
Ruben <Ruben@2A5PC17.fpmislata.local>
2013-12-18 11:01:35 +0100

    Poner anotaciones en la clase usuario.

--- src/java/com/pymegest/applicationserver/dominio/Usuario.java
+++ src/java/com/pymegest/applicationserver/dominio/Usuario.java
@@ -27,20 +27,40 @@
 package com.pymegest.applicationserver.dominio;
 
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
- * @author Miguel María Martínez Echeverría
+ * @author Juanjo
  * @version 1.0
  * @since 1.0
  */
-public class Usuario implements Serializable {
 
+@Entity
+@Table(name="usuario")
+public class Usuario implements Serializable {
+ 
+    @Id
+    @Column(name="id_usuario")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer idUsuario;
+    
+    @Enumerated(EnumType.ORDINAL)
     private TipoCuenta tipoCuenta;
+
+    public Usuario() {
+    }
     
-    
-    
-    
+    public Usuario(Integer idUsuario, TipoCuenta tipoCuenta) {
+        this.idUsuario = idUsuario;
+        this.tipoCuenta = tipoCuenta;
+    }   
 
     /**
      * @return the idUsuario
